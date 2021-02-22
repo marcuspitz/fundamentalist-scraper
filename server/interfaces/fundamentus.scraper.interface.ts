b475d0c81ba70e9010036672b7ad2c4493c0e8e7
Marcus Pitz <marcusviniciuspitz@gmail.com>
2021-02-22 20:38:08 -0300

    Stock details stuff

--- server/interfaces/fundamentus.scraper.interface.ts
+++ server/interfaces/fundamentus.scraper.interface.ts
@@ -1,4 +1,6 @@
+import { StockDetails } from "./stock.details.interface";
 
 export interface IFundamentusScraperService {
     getListOfStocks(): Promise<string[]>;
+    getStockDetail(stockCode: string): Promise<StockDetails>;
 }
--- server/services/fundamentus.scraper.service.ts
+++ server/services/fundamentus.scraper.service.ts
@@ -1,7 +1,8 @@
 import { IFundamentusScraperService } from "../interfaces/fundamentus.scraper.interface";
 import { parameterAsString } from "../utilities/parameters";
 import * as request from "request-promise";
-import { copyAllContent } from "../utilities/html.utilities";
+import { copyAllContent, copyContent } from "../utilities/html.utilities";
+import { StockDetails } from "../interfaces/stock.details.interface";
 
 export class FundamentusScraperService implements IFundamentusScraperService {
     
@@ -19,6 +20,20 @@ export class FundamentusScraperService implements IFundamentusScraperService {
         });
         const stocks = copyAllContent(`detalhes.php?papel=`, '"', content);
         return stocks.filter(s => s.trim().length > 0);
+    }
+
+    public async getStockDetail(stockCode: string): Promise<StockDetails> {
+        const content: string = await request.get(`${this.baseStockUrl}?papel=${stockCode}`, {
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+        });
+        return this.parseStockContent(content);
+    }
+
+    private parseStockContent(content: string): StockDetails {
+        let price: number;
+        copyContent(`<span class="txt">`, `</span>`, copyContent(`</span><span class="txt">Cotação</span></td>`, `</td>`, content))
     }
 
 }
